abac5a9ba89dfb28950f1c84f95b58988fdf56d6
ogungbile oluwasogo <ogungbileshogo@gmail.com>
2024-12-25 01:22:42 -0800

    Contact us page

--- src/Components/ContactUsComponent/ContactBody.jsx
+++ src/Components/ContactUsComponent/ContactBody.jsx
@@ -3,14 +3,30 @@ const ContactBody = () => {
   return (
     <div>
       <div className={styles.flexContact}>
-        <div>
+        <div className={styles.contact1}>
             <div className={styles.sideBorder}></div>
             <h2>Let’s Get In Touch</h2>
             <h3>Stay Tuned & Keep Connected With Us...........</h3>
         </div>
         <div className={styles.contact2}>
-            
+            <form action="">
+                <div>
+                    <input type="text" placeholder='First Name' />
+                    <input type="text" placeholder='Last Name' />
+                </div>
+                <div>
+                    <input type="text" placeholder='Subject' />
+                    <input type="email" placeholder='Email' />
+                </div>
+                <div>
+                <textarea name="">Message</textarea>
+                </div>
+                <button type='button'>REACH OUT</button>
+            </form>
         </div>
+      </div>
+      <div className={styles.contactImage}>
+        <img src="/map_contact.png" alt="" />
       </div>
     </div>
   )
