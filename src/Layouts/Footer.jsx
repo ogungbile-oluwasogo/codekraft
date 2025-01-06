2c15d601ab9d0690b9dd644912e11f158bf3e226
ogungbile oluwasogo <ogungbileshogo@gmail.com>
2025-01-06 09:18:56 -0800

    adjustment

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -14,24 +14,24 @@ const Footer = () => {
           </div>
           <div>
             <h2>MENUS</h2>
-            <Link to='/'><Arrow /> HOME</Link>
-            <Link to='/'><Arrow />  ABOUT US</Link>
-            <Link to='/'><Arrow />  PACKAGES</Link>
-            <Link to='/'><Arrow />  SERVICES</Link>
-            <Link to='/'><Arrow />  CONTACT US</Link>
+            <Link to='/'><Arrow /><p>HOME</p> </Link>
+            <Link to='/'><Arrow />  <p>ABOUT US</p></Link>
+            <Link to='/'><Arrow />  <p>PACKAGES</p></Link>
+            <Link to='/'><Arrow />  <p>SERVICES</p></Link>
+            <Link to='/'><Arrow />  <p>CONTACT US</p></Link>
           </div>
           <div>
             <h2>NAVIGATION</h2>
-            <Link to='/'><Arrow /> Graphic Design</Link>
-            <Link to='/'><Arrow />  Web Development</Link>
-            <Link to='/'><Arrow />  Ui/ux Design</Link>
-            <Link to='/'><Arrow />  Digital Marketing</Link>
+            <Link to='/'><Arrow /> <p>Graphic Design</p></Link>
+            <Link to='/'><Arrow />  <p>Web Development</p></Link>
+            <Link to='/'><Arrow />  <p>Ui/ux Design</p></Link>
+            <Link to='/'><Arrow />  <p>Digital Marketing</p></Link>
           </div>
           <div>
             <h2>SITE POLICY</h2>
-            <Link to='/'><Arrow />   Privacy & Policy</Link>
-            <Link to='/'><Arrow />  Terms of services</Link>
-            <Link to='/'><Arrow />  Disclaimer</Link>
+            <Link to='/'><Arrow />   <p>Privacy & Policy</p></Link>
+            <Link to='/'><Arrow />  <p>Terms of services</p></Link>
+            <Link to='/'><Arrow />  <p>Disclaimer</p></Link>
           </div>
         </div>
       </div>
