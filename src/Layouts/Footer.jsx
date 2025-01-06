1bebf2c6475c069035112ace0b47badb3513690f
ogungbile oluwasogo <ogungbileshogo@gmail.com>
2025-01-06 09:32:10 -0800

    adjustment

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -10,28 +10,29 @@ const Footer = () => {
         <div className={styles.footerGrid}>
           <div className={styles.social}>
           <Logo />
+          <br /><br />
           <SocialMedia />
           </div>
           <div>
             <h2>MENUS</h2>
             <Link to='/'><Arrow /><p>HOME</p> </Link>
-            <Link to='/'><Arrow />  <p>ABOUT US</p></Link>
-            <Link to='/'><Arrow />  <p>PACKAGES</p></Link>
-            <Link to='/'><Arrow />  <p>SERVICES</p></Link>
-            <Link to='/'><Arrow />  <p>CONTACT US</p></Link>
+            <Link to='/'><Arrow /><p>ABOUT US</p></Link>
+            <Link to='/'><Arrow /><p>PACKAGES</p></Link>
+            <Link to='/'><Arrow /><p>SERVICES</p></Link>
+            <Link to='/'><Arrow /><p>CONTACT US</p></Link>
           </div>
           <div>
             <h2>NAVIGATION</h2>
-            <Link to='/'><Arrow /> <p>Graphic Design</p></Link>
-            <Link to='/'><Arrow />  <p>Web Development</p></Link>
-            <Link to='/'><Arrow />  <p>Ui/ux Design</p></Link>
-            <Link to='/'><Arrow />  <p>Digital Marketing</p></Link>
+            <Link to='/'><Arrow /><p>Graphic Design</p></Link>
+            <Link to='/'><Arrow /><p>Web Development</p></Link>
+            <Link to='/'><Arrow /><p>Ui/ux Design</p></Link>
+            <Link to='/'><Arrow /><p>Digital Marketing</p></Link>
           </div>
           <div>
             <h2>SITE POLICY</h2>
-            <Link to='/'><Arrow />   <p>Privacy & Policy</p></Link>
-            <Link to='/'><Arrow />  <p>Terms of services</p></Link>
-            <Link to='/'><Arrow />  <p>Disclaimer</p></Link>
+            <Link to='/'><Arrow /><p>Privacy & Policy</p></Link>
+            <Link to='/'><Arrow /><p>Terms of services</p></Link>
+            <Link to='/'><Arrow /><p>Disclaimer</p></Link>
           </div>
         </div>
       </div>
